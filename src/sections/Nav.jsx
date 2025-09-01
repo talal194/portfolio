15b1e55b80bd0535e8994276bedb922de63b7ed9
Talal Ahmed <ahmedtalal367@gmail.com>
2025-09-01 12:14:37 +0500

    links are working

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -9,14 +9,14 @@ export default function Nav(){
 return(<section className="container">
     <nav className="navbar navbar-expand-lg">
     <div className="container-fluid">
-      <Link className="navbar-brand" to="/"></Link>
+      <Link className="navbar-brand" to="#"></Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            <Link className="nav-link active" aria-current="page" to="hero" smooth={true} duration={500}>Home</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to="about" smooth={true} duration={500}>About</Link>
@@ -27,9 +27,9 @@ return(<section className="container">
           <li className="nav-item">
             <Link className="nav-link" to="projects" smooth={true} duration={500}>Projects</Link>
           </li>
-          {/* <li className="nav-item">
-            <Link className="nav-link" to="/blog" >Blog</Link>
-          </li> */}
+          <li className="nav-item">
+            <Link className="nav-link" to="contact" >Contact</Link>
+          </li>
         </ul>
       </div>
     </div>
